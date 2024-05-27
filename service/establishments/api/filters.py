41c6feb9b2c5e47bb979a4fe49beb469ab1e3fa9
Kapitoshka-a <taraspanechko737@gmail.com>
2024-05-27 12:29:09 +0300

    make filters take multiple parameters

--- service/establishments/api/filters.py
+++ service/establishments/api/filters.py
@@ -3,12 +3,24 @@ from .models import Establishment
 
 
 class EstablishmentFilter(django_filters.FilterSet):
-    amenities = django_filters.CharFilter(field_name='amenities__name', lookup_expr='icontains')
-    services = django_filters.CharFilter(field_name='services__name', lookup_expr='icontains')
-    name = django_filters.CharFilter(lookup_expr='icontains')
+    amenities = django_filters.CharFilter(method='filter_amenities')
+    services = django_filters.CharFilter(method='filter_services')
     type = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Establishment
-        fields = ['name', 'type', 'amenities', 'services', 'is_recommended',
-                  'address__city', 'price_category']
+        fields = ['type', 'amenities', 'services', 'is_recommended', 'price_category', 'capacity']
+
+    def filter_amenities(self, queryset, name, value):
+        amenities_list = value.split(';')
+        queryset = queryset
+        for amenity in amenities_list:
+            queryset = queryset.filter(amenities__name__icontains=amenity)
+        return queryset
+
+    def filter_services(self, queryset, name, value):
+        services_list = value.split(';')
+        queryset = queryset
+        for amenity in services_list:
+            queryset = queryset.filter(services__name__icontains=amenity)
+        return queryset
